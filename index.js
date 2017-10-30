571bacf21a9fb20607f60f40403be6674d4370b6
Yidan Wang <wangyidan621@gmail.com>
2017-10-30 21:22:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ function sayHiToGrandma(string){
   } else if (string === string.toLowerCase()){
     return "I can't hear you!";
   } else if (string ==="I love you, Grandma." ){
-    return "I love you, Grandma.";
+    return "I love you, too.";
   }
 }
